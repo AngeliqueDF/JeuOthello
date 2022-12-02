ce698d1a4a90f48adcf472d2c7f2cc3b99193ef8
angelique-df <faye.angelique.d@gmail.com>
2022-12-02 17:24:30 +0000

    Update Plateau.java
    - Add `retournerJeton` method

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,3 +1,9 @@
+class RetournerJetonCaseVideException extends Exception {
+    public RetournerJetonCaseVideException(String message) {
+        super(message);
+    }
+}
+
 public class Plateau {
     boolean configInitiale;
     private Jeton[][] cases = new Jeton[8][8];
@@ -11,6 +17,16 @@ public class Plateau {
 
             this.cases[3][3] = new Jeton('B');
             this.cases[4][4] = new Jeton('B');
+        }
+    }
+
+    void retournerJeton(String coord) throws RetournerJetonCaseVideException {
+        int colonne = Character.getNumericValue(coord.charAt(0)) - 10;
+        int rangee = Character.getNumericValue(coord.charAt(1)) - 1;
+        if (this.cases[rangee][colonne] != null) {
+            this.cases[rangee][colonne].retournerJeton();
+        } else {
+            throw new RetournerJetonCaseVideException("Erreur : cette case ne contient aucun jeton.");
         }
     }
 
