cb30a996593cec048c87ca14df2e6a226d66da83
angelique-df <faye.angelique.d@gmail.com>
2022-12-02 17:22:23 +0000

    Update Jeton.java
    - Add method to toggle pawn color

--- src/Jeton.java
+++ src/Jeton.java
@@ -5,6 +5,15 @@ public class Jeton {
         this.face = faceInitiale;
     }
 
+    void retournerJeton() {
+        System.out.println(this.face);
+        if (this.face == 'N') {
+            this.face = 'B';
+        } else if (this.face == 'B') {
+            this.face = 'N';
+        }
+    }
+
     @Override
     public String toString() {
         return "" + this.face;
