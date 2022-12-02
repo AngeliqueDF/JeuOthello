295271e4a0e12152d1d5c72dc8614edfe388bc99
angelique-df <faye.angelique.d@gmail.com>
2022-12-02 16:56:17 +0000

    Update Plateau.java
    - Add constructor, attributes
    - Conditionally set initial configuration

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,2 +1,17 @@
 public class Plateau {
+    boolean configInitiale;
+    private Jeton[][] cases = new Jeton[8][8];
+
+    Plateau(boolean configInitiale) {
+        if (configInitiale) {
+            this.configInitiale = true;
+            // deux noirs, en e4 et d5, et deux blancs, en d4 et e5. Chaque joueur, noir et blanc, pose l'un après l'autre un pion de sa couleur sur
+            this.cases[4][3] = new Jeton('N');
+            this.cases[3][4] = new Jeton('N');
+
+            this.cases[3][3] = new Jeton('B');
+            this.cases[4][4] = new Jeton('B');
+        }
+    }
+
 }
